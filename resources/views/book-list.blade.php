1933f6622714e544884514f76c1f4f3a6e373d64
Nadzirah <nadzirahnad5@gmail.com>
2023-02-07 00:59:16 +0800

    search books

--- resources/views/book-list.blade.php
+++ resources/views/book-list.blade.php
@@ -4,9 +4,27 @@
  
 @section('content')
 
-<div>
-    <h2>Books</h2>
-</div>
+<form action="" method="get">
+    <div class="row">
+        <div class="col-12 col-sm-6">
+            <select name="category" id="category" class="form-control">
+                <option value="">Select category</option>
+                @foreach ($categories as $item)
+                    <option value="{{ $item->id }}">{{ $item->name }}</option>
+                @endforeach
+            </select>
+        </div>
+    
+        <div class="col-12 col-sm-6">
+            <div class="input-group mb-3">
+                <input type="text" name="title" id="title" class="form-control" placeholder="Book's title">
+                <button class="btn btn-primary" type="submit">Search</button>
+            </div>
+        </div>
+    </div>
+
+</form>
+
 
 <div class="my-5">
     <div class="row">
@@ -19,6 +37,7 @@
                                 alt="..." draggable="false">
                 <div class="card-body">
                     <h5 class="card-title">{{ $item->title }}</h5>
+                    <p class="card-text">{{ $item->book_code }}</p>
                     <p class="card-text">{{ $item->book_code }}</p>
                     <p class="fw-bold card-text text-end 
                         {{ $item->status == 'In stock' ? 'text-success' : 'text-danger' }}">
